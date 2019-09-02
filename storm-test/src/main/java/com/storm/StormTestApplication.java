f12dd11c15802c74e80ee87615b45ac61dc35408
liangqifeng <liangqifeng@zhibankeji.com>
2019-09-02 20:43:57 +0800

    初始化springboot

---
+++ storm-test/src/main/java/com/storm/StormTestApplication.java
@@ -0,0 +1,13 @@
+package com.storm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StormTestApplication.class, args);
+	}
+
+}
